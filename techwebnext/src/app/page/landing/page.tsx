d1d25a000d5bea7345ebb66c1039d2f91d6ef758
Anaia-Randrianantenaina <anaiarandrianantenaina@gmail.com>
2025-05-14 21:01:06 +0300

    responsive design

--- techwebnext/src/app/page/landing/page.tsx
+++ techwebnext/src/app/page/landing/page.tsx
@@ -8,21 +8,21 @@ export default function Landing (){
            <div>
            <Sidebar />
             {/* BANNIÈRE DE PUB */}
-            <div className="sm:flex sm:justify-center sm:items-center sm:mt-9">
-                <div className="w-[80%] sm:h-[550px]  md:h-[350px]  bg-gray-200  rounded-3xl ">
-                    <div>
-                        <div className="flex justify-center items-center">
-                            <img src="image/6.png" alt="" className="w-[250px]"/>
-                        </div>
-                        <div className=" sm:text-center">
-                            <p className="font-semibold text-orange-600 text-[40px]">25% OFF</p>
-                            <p className="font-semibold text-[50px]">Summer Sale</p>
-                            <p>Discover our summer styles with discount</p>
-                        </div>
-                    </div>
-                    <div className="">
-                        <Boutton /> 
-                    </div>
+            <div className="flex justify-center items-center mt-8">
+                 <div className="w-[600px] h-[500px] bg-gray-200 lg:w-[70%] lg:h-[300px] rounded-2xl">
+                   <div className="space-y-4 lg:flex lg:flex-row-reverse justify-between lg:mt-10">
+
+                     <div className="flex justify-center items-center mt-2 lg:mr-70">
+                        <img src="/image/6.png" alt=""  className="w-72 lg:w-[350px]"/>
+                     </div>
+                     <div className=" space-y-5 text-center lg:ml-70">
+                        <p className="text-orange-600 font-semibold text-3xl">25% OFF</p>
+                        <p className="font-semibold text-5xl">Summer Sale</p>
+                        <p className="text-gray-600">Discover our summer styles with discount</p>
+                        <Boutton />
+                     </div>
+
+                   </div>
                 </div>
             </div>
            </div>
